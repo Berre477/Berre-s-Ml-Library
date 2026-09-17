ffbfadc43c451f71a69a7a373a5091de11f7433c
Berre47 <alexandervhome@gmail.com>
2026-09-17 09:51:26 +0200

    Added MinMaxScaler

--- minilearn/preprocessing/scalers.py
+++ minilearn/preprocessing/scalers.py
@@ -14,3 +14,22 @@ class StandartScaler:
     
     def transform(self,X):
         return (X -self.mean) / (self.std + self.eps)
+
+
+class MinMaxScaler:
+    def __init__(self,dtype=np.float32):
+        self.eps = np.finfo(dtype).eps
+        self.min = None
+        self.max =None
+
+    def fit(self,X):
+        self.min = np.min(X,axis=0)
+        self.max = np.max(X,axis=0)
+        return self
+
+    def transforme(self,X):
+        return (X - self.min)/ (self.max - self.min + self.eps)
+
+    def fit_transform(self,X):
+        return self.fit(X).transforme(X)
+            
