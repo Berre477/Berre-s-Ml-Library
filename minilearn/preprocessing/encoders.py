2dd73a14f82eb75daec9e8fc5e92fc6c73406fcb
Berre47 <alexandervinhome@gmail.com>
2026-09-23 14:43:51 +0200

    Added label encoder

---
+++ minilearn/preprocessing/encoders.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class LabelEnoder:
+    def __init__(self):
+        self.classes_ = None
+        self._mapping = None
+        self._imapping = None
+
+    def fit(self,y):
+        self.classes_ = np.unique(y)
+        self._mapping = {label: idx for idx, label in enumerate(self.classes_)}
+        self._inv_mapping = {idx: label for label, idx in self._mapping.items()}
+        return self
+
+    def transform(self,y):
+        return np.array([self._mapping[label] for label in y], dtype=np.int64)
+
+    def fit_transform(self,y):
+        return self.fit(y).transform(y)
+
+    def inverse_transform(self,y_indeices):
+        return np.array([self._inv_mapping[idx] for idx in y_indices])
