283a8e109a64c1362a70af721834e6d79e97ca32
Berre47 <alexandervinhome@gmail.com>
2026-09-24 23:03:40 +0200

    Started the OrdinalEncoder

--- minilearn/preprocessing/encoders.py
+++ minilearn/preprocessing/encoders.py
@@ -18,5 +18,28 @@ class LabelEnoder:
     def fit_transform(self,y):
         return self.fit(y).transform(y)
 
-    def inverse_transform(self,y_indeices):
+    def inverse_transform(self,y_indices):
         return np.array([self._inv_mapping[idx] for idx in y_indices])
+
+
+
+
+class OrdinalEncoder:
+
+    def __init__(self,categories="auto"):
+        self.categories = categories
+        self.categories_ = []
+        self.mapping_ = []
+
+    def fit(self,X):
+        X = np.asarray(X)
+        n_featues = X.shape[1]
+        self.categories_ = []
+        self.mapping_ = []
+
+    def transform(self,X):
+        X = np.asarray(X)
+        X_out = np.zeros(X.shape,dtype=np.float32)
+        
+        
+        
