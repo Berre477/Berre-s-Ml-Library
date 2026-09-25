333ebcf38e552370fb95f88e296072e970c1f92b
Berre47 <alexandervinhome@gmail.com>
2026-09-25 20:22:16 +0200

    Added One hot encoder

--- minilearn/preprocessing/encoders.py
+++ minilearn/preprocessing/encoders.py
@@ -37,9 +37,51 @@ class OrdinalEncoder:
         self.categories_ = []
         self.mapping_ = []
 
+        for col in range(n_featues):
+            if self.categories == "auto":
+                cats = np.unique(X[:,col])
+
+            else:
+                cats = np.array(self.categories[col])
+
+            self.categories_.append(cats)
+            self.mapping_.append({cat: idx for idx,cat in enumerate(cats)})
+        return self
+
     def transform(self,X):
         X = np.asarray(X)
         X_out = np.zeros(X.shape,dtype=np.float32)
+
+
         
-        
-        
+
+class OneHotEncoder :
+    def __init__(self,handle_unknown ='ignore'):
+        self.categories_ = []
+        self.handle_unknown = handle_unknown
+
+    def fit(self,X):
+        X = np.asarray(X)
+        self.categories_ = [np.unique(X[:,col]) for col in range(X.shape[1])]
+        return self
+
+    def transform(self,X):
+        X = np.asarray(X)
+        encoded_blocks = []
+
+        for col_idx,categories in enumerate(self.categories_):
+            col_data = X[:,col_idx]
+            num_categories = len(categories)
+            mapping = {cat: idx for idx, cat in enumerate(categories)}
+            one_hot_block = np.zeros((len(col_data), num_categories), dtype=np.float32)
+
+            for row_idx,val in enumerate(col_data):
+                if val in mapping:
+                    one_hot_block[row_idx,mapping[val]] = 1.0
+                elif self.handle_unknown == "errr":
+                    raise ValueError(f"Unknown category {val} is in the column {col_idx}")
+
+            encoded_blocks.append(one_hot_block)
+
+
+        return np.hstack(encoded_blocks)
